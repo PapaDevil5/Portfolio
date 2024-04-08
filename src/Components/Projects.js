969a1a36fbff0a671d4c3321776ce4f30791840c
PapaDevil5 <guptaalankrit98@gmail.com>
2024-04-09 01:10:18 +0530

    second commit

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -25,19 +25,19 @@ function Projects() {
                     <h3 class="card-title">Living</h3>
                     <p class="card-text">The mobile application project "CREATE LIVING" is developed with the aim of developing a Hostel/PG management application which will prevent food, resources and time wastage.
                         </p>
-                    <div className='button'><a href="" className="btn btn-danger">See Project</a></div>
+                    <div className='button'><a href="https://github.com/PapaDevil5/Hostel" target='_blank'  className="btn btn-danger">See Project</a></div>
                 </div>
                 <div class="card">
                     <img src={playstation} class="card-img-top" alt="Project 3"></img>
                     <h3 class="card-title">PlayStation</h3>
                     <p class="card-text">At PlayStation Clone, we have meticulously crafted a website that mirrors the aesthetics and functionalities of the original PlayStation interface, while embracing modern design principles.</p>
-                    <div className='button'><a href="" className="btn btn-danger">See Project</a></div>
+                    <div className='button'><a href="https://github.com/PapaDevil5/PlayStation-Clone" target="_blank" className="btn btn-danger">See Project</a></div>
                 </div>
                 <div class="card">
                     <img src={vidglimpse} class="card-img-top" alt="Project 1"></img>
                     <h3 class="card-title">VidGlimpse</h3>
                     <p class="card-text">VidGlimpse introduces an advanced video chat platform developed using WebRTC principles alongside HTML, CSS, and JavaScript technologies. Leveraging WebRTC ensures robust, secure peer-to-peer connections.</p>
-                    <div className='button'><a href="" className="btn btn-danger">See Project</a></div>
+                    <div className='button'><a href="https://github.com/PapaDevil5/VidGlimpse" target ='_blank' className="btn btn-danger">See Project</a></div>
                 </div>
             </div>
                
