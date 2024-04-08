11429861f3afca2c22e63f4142b7bdbdd0c98106
PapaDevil5 <guptaalankrit98@gmail.com>
2024-04-09 00:24:57 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Contact from './Components/Contact';
+import Content from './Components/Content';
+import NavBar from './Components/NavBar';
+import Projects from './Components/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <NavBar/>
+    <Content/>
+    <Projects/>
+    <Contact/>
+    </>
   );
 }
 
